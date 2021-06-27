b762998a394920c5ad02d90081dfb8755081de42
josdem <joseluis.delacruz@gmail.com>
2021-06-27 10:23:44 -0400

    Starting new Spring Boot SOAP demo project :)

---
+++ src/main/java/com/jos/dem/soap/SoapServerApplication.java
@@ -0,0 +1,13 @@
+package com.jos.dem.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServerApplication.class, args);
+	}
+
+}
