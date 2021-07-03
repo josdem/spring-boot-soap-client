7a6eea446b734bf7238b183761139f958781b126
josdem <joseluis.delacruz@gmail.com>
2021-07-03 11:59:53 -0400

    Adding WSLite groovy request

---
+++ groovyRequest.groovy
@@ -0,0 +1,17 @@
+@Grab('com.github.groovy-wslite:groovy-wslite:1.1.3')
+
+
+import wslite.soap.SOAPClient
+
+File file = new File('request.xml')
+assert file.exists()
+
+String url = 'http://localhost:8080/ws'
+
+def client = new SOAPClient(url)
+def response = client.send(file.text)
+
+assert 200 == response.httpResponse.statusCode
+
+println response.dump()
+print response.text
